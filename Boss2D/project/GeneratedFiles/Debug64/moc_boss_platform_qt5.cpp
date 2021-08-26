5116ecb6cf5425d5bcfd14389ab9ee1ba2b49198
Bonex Goo <bonexgoo@gmail.com>
2021-08-26 18:39:41 +0900

    3.1.1
    QT moc리빌드

--- Boss2D/project/GeneratedFiles/Debug64/moc_boss_platform_qt5.cpp
+++ Boss2D/project/GeneratedFiles/Debug64/moc_boss_platform_qt5.cpp
@@ -3574,6 +3574,76 @@ int GenericView::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     _id = FramePrivate::qt_metacall(_c, _id, _a);
     return _id;
 }
+struct qt_meta_stringdata_GenericViewGL_t {
+    QByteArrayData data[1];
+    char stringdata0[14];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_GenericViewGL_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_GenericViewGL_t qt_meta_stringdata_GenericViewGL = {
+    {
+QT_MOC_LITERAL(0, 0, 13) // "GenericViewGL"
+
+    },
+    "GenericViewGL"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_GenericViewGL[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       0,    0, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+       0        // eod
+};
+
+void GenericViewGL::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    Q_UNUSED(_o);
+    Q_UNUSED(_id);
+    Q_UNUSED(_c);
+    Q_UNUSED(_a);
+}
+
+QT_INIT_METAOBJECT const QMetaObject GenericViewGL::staticMetaObject = { {
+    QMetaObject::SuperData::link<GLWidgetPrivate::staticMetaObject>(),
+    qt_meta_stringdata_GenericViewGL.data,
+    qt_meta_data_GenericViewGL,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *GenericViewGL::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *GenericViewGL::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_GenericViewGL.stringdata0))
+        return static_cast<void*>(this);
+    return GLWidgetPrivate::qt_metacast(_clname);
+}
+
+int GenericViewGL::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = GLWidgetPrivate::qt_metacall(_c, _id, _a);
+    return _id;
+}
 struct qt_meta_stringdata_MainViewGL_t {
     QByteArrayData data[1];
     char stringdata0[11];
