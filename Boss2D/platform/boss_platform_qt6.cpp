c274d7fd14c6acfdbe8bd5143b5e9029c9048f62
BonexGoo <bonexgoo@gmail.com>
2026-05-27 19:57:46 +0900

    5.15.3
    RLottie의 라즈베리 컴파일옵션 수정

--- Boss2D/platform/boss_platform_qt6.cpp
+++ Boss2D/platform/boss_platform_qt6.cpp
@@ -260,6 +260,28 @@
         #endif
     }
 
+    #if BOSS_HAVE_RLOTTIE && BOSS_LINUX && defined(__aarch64__) && !defined(__x86_64__)
+        extern "C"
+        {
+            long int __isoc23_strtol(const char* nptr, char** endptr, int base)
+            {
+                return strtol(nptr, endptr, base);
+            }
+            unsigned long int __isoc23_strtoul(const char* nptr, char** endptr, int base)
+            {
+                return strtoul(nptr, endptr, base);
+            }
+            long long int __isoc23_strtoll(const char* nptr, char** endptr, int base)
+            {
+                return strtoll(nptr, endptr, base);
+            }
+            unsigned long long int __isoc23_strtoull(const char* nptr, char** endptr, int base)
+            {
+                return strtoull(nptr, endptr, base);
+            }
+        }
+    #endif
+
     CanvasClass::CanvasClass() : mIsTypeSurface(true)
     {
         mIsSurfaceBinded = false;
